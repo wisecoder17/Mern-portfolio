8b5505c300868a6bf9b4ec338ec3b86075ef3836
wisecoder17 <richardogunwole17@gmail.com>
2024-06-24 10:05:22 +0100

    api callback

--- routes/portfolioRoute.jsx
+++ routes/portfolioRoute.jsx
@@ -1,2 +1,30 @@
 const router = require('express').Router();
-const {Intro, About, Experience, Portfolio, Services, Contact} = require ('../models/portfolioModel.jsx');
+const { Intro, About, Experience, Portfolio, Services, Contact } = require('../models/portfolioModel.jsx');
+
+
+router.get('/get-portfolio-data', async (req, res) => {
+
+    try {
+        const intros = await Intro.find();
+        const abouts = await About.find();
+        const experiences = await Experience.find();
+        const portfolios = await Portfolio.find();
+        const services = await Services.find();
+        const contacts = await Contact.find();
+
+        res.status(200).send({
+            intro: intros[0],
+            about: abouts[0],
+            experience: experiences[0],
+            portfolio: portfolios[0],
+            service: services[0],
+            contact: contacts[0],
+        });
+
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+module.exports = router;
--- server.jsx
+++ server.jsx
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const dbConfig = require('./config/dbConfig.jsx');
+
+const portfolioRoute = require('./routes/portfolioRoute.jsx');
+
+app.use(express.json());
+
+app.use("/api/portfolio", portfolioRoute);
+
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
