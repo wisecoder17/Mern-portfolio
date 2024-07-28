8344d191e532e5527f141ab61ea803fb6e914479
wisecoder17 <richardogunwole17@gmail.com>
2024-07-28 22:20:14 +0100

    Update server.jsx

--- server.jsx
+++ server.jsx
@@ -1,15 +1,16 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-const dbConfig = require('./config/dbConfig.jsx');
+// const express = require('express');
+// const app = express();
+// require('dotenv').config();
+// const dbConfig = require('./config/dbConfig.jsx');
 
-const portfolioRoute = require('./routes/portfolioRoute.jsx');
+// const portfolioRoute = require('./routes/portfolioRoute.jsx');
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use("/api/portfolio", portfolioRoute);
+// app.use("/api/portfolio", portfolioRoute);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
+
