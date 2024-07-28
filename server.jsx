560157ec6fa9988967827a6cab68e0610d5f1886
wisecoder17 <richardogunwole17@gmail.com>
2024-07-28 22:21:56 +0100

    Update server.jsx

--- server.jsx
+++ server.jsx
@@ -1,61 +1,15 @@
-// const express = require('express');
-// const app = express();
-// require('dotenv').config();
-// const dbConfig = require('./config/dbConfig.jsx');
-
-// const portfolioRoute = require('./routes/portfolioRoute.jsx');
-
-// app.use(express.json());
-
-// app.use("/api/portfolio", portfolioRoute);
-
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
-
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const dbConfig = require('./config/dbConfig.jsx');
 
-// Serve static files (like your HTML file)
-app.use(express.static('public'));
+const portfolioRoute = require('./routes/portfolioRoute.jsx');
 
-// Configure multer for file uploads
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const uploadDir = 'uploads/';
-        // Ensure the upload directory exists
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir);
-        }
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        const pre = "FBI"
-        const filename = path.basename(file.originalname, ext);
-        cb(null, `${pre}-${Date.now()}${ext}`);
-    }
-});
-const upload = multer({ storage });
+app.use(express.json());
 
-// Serve the signup form
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,  'img.html'));
-});
+app.use("/api/portfolio", portfolioRoute);
 
-// Handle file uploads
-app.post('/upload', upload.single('profileImage'), (req, res) => {
-    // `req.file` contains information about the uploaded file
-    console.log('File uploaded:', req.file);
-    res.send('File uploaded successfully!');
-});
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
